8726605f4b6856d46693aea38c24d06584b37fa6
Rafael Gonzaga via Xcode <rluisp@icloud.com>
2023-07-30 15:33:58 -0300

    Modificar e estilizar as Views

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -9,24 +9,40 @@ import SwiftUI
 
 struct MeetingView: View {
     var body: some View {
+        
         VStack {
             
-         ProgressView( value: 5, total: 10)
+            ProgressView(value: 5, total: 10)
             
             HStack {
-                
-                VStack {
+                VStack(alignment: .leading) {
                     Text("Seconds Elapsed")
+                        .font(.caption)
                     Label("300", systemImage: "hourglass.tophalf.fill")
                 }
                 
-                VStack {
+                Spacer()
+                
+                VStack(alignment: .trailing) {
                     Text("Seconds Remaining")
+                        .font(.caption)
+                    
                     Label("600", systemImage: "hourglass.bottomhalf.fill")
                 }
             }
+            
+            Circle()
+                .strokeBorder(lineWidth: 24)
+            
+            HStack {
+                Text("Speaker 1 of 3")
+                
+                Button(action: {}) {
+                    Image(systemName: "foward.fill")
+                }
+            }
         }
-    
+        .padding()
     }
 }
 
