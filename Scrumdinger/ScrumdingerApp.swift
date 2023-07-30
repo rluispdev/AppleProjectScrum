c9786e515b2e219da1e5e22370b2d4423689e0e5
Rafael Gonzaga via Xcode <rluisp@icloud.com>
2023-07-30 14:50:06 -0300

    Adicionar o commit Inicial

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Rafael Gonzaga on 30/07/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
