afadab7f3f191fc2092389dcbb3253afe6b5810a
Rafael Gonzaga via Xcode <rluisp@icloud.com>
2023-09-02 16:58:18 -0300

    Adicionar participantes na DetailEditView.

--- Scrumdinger/Views/DetailEditView.swift
+++ Scrumdinger/Views/DetailEditView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct DetailEditView: View {
     ///@State  como privado para que possam ser acessadas só pelas views  que  definirmos.
     @State private var scrum = DailyScrum.emptyScrum
+    @State private var newAttendeeName = ""
     
     var body: some View {
         Form {
@@ -22,6 +23,28 @@ struct DetailEditView: View {
                     }
                     Spacer()
                     Text("\(scrum.lengthInMinutes) minutes")
+                }
+                
+            }
+            Section(header: Text("Attendees")){
+                ForEach(scrum.attendees) { attendee in
+                    Text(attendee.name)
+                }
+                .onDelete { indices in
+                    scrum.attendees.remove(atOffsets: indices)
+                }
+                HStack{
+                    TextField("New Attendee", text: $newAttendeeName)
+                    Button(action: {
+                        withAnimation {
+                            let attendee = DailyScrum.Attendee(name: newAttendeeName)
+                            scrum.attendees.append(attendee)
+                            newAttendeeName = ""
+                        }
+                    }) {
+                        Image(systemName: "plus.circle.fill")
+                    }
+                    .disabled(newAttendeeName.isEmpty)
                 }
             }
         }
